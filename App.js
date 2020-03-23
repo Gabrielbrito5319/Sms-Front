a7c5b3419d60f819e88451722b4caee297d62cd7
Gabrielbrito5319 <48894548+Gabrielbrito5319@users.noreply.github.com>
2020-03-23 20:09:38 -0300

    Add files via upload
    teste10

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { HomePage } from './Home/HomePage';
+function App() {
+  return (
+    <div className="app d-flex align-items-center justify-content-center h-100">
+      <HomePage/>
+    </div>
+  );
+}
+
+export default App;
